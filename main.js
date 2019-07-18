b06471ae18e5ead776c52bc94f64ddc524a7f9e0
Walter Satriano <walter.satri@gmail.com>
2019-07-18 19:36:37 +0200

    creazione eventi on-click su ogni dropdown menù

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 $( document ).ready(function() {
 
+  //cambio il colore del testo a Personal e Business al click
+  $(".left_items a:not(:first-child)").click(
+    function(){
+      $(this).toggleClass("text_black");
+    }
+  );
+
+  //cambio il colore del testo Pricing al click
+  $(".right_items a.price").click(
+    function(){
+      $(".price").toggleClass("text_black");
+    }
+  );
 
   //drop menù su Accounts
   $(".right_items a.acc").click(
     function(){
       $("#drop_accounts").toggleClass("active");
+      $(".acc").toggleClass("text_black");
     }
   );
 
@@ -12,6 +26,7 @@ $( document ).ready(function() {
   $(".right_items a.prod").click(
     function(){
       $("#drop_products").toggleClass("active");
+      $(".prod").toggleClass("text_black");
     }
   );
 
@@ -19,6 +34,7 @@ $( document ).ready(function() {
   $(".right_items a.comp").click(
     function(){
       $("#drop_company").toggleClass("active");
+      $(".comp").toggleClass("text_black");
     }
   );
 
@@ -26,6 +42,7 @@ $( document ).ready(function() {
   $(".right_items a.help").click(
     function(){
       $("#drop_help").toggleClass("active");
+      $(".help").toggleClass("text_black");
     }
   );
 
@@ -33,6 +50,7 @@ $( document ).ready(function() {
   $(".right_items a.lang").click(
     function(){
       $("#drop_en").toggleClass("active");
+      $(".lang").toggleClass("text_black");
     }
   );
 
